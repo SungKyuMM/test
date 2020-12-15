c43c8b284e60d3411ced04afcdf77dfedb784f04
leejongsoo1 <jongsoo996@gmail.com>
2020-12-15 23:59:29 +0900

    이종수

--- comoa/controller/infectionStatusController.js
+++ comoa/controller/infectionStatusController.js
@@ -3,8 +3,7 @@ const infectionMongo = require('../mongoDB/infectionStatusMongo');
 module.exports = {
     infectionGraph: async (req, res, next) => {
         let data = {create_dt: {$gte: new Date("2020-11-01")}};
-        let infectionData = await infectionMongo.find(data);
-        
+        let infectionData = await infectionMongo.find(data);        
         res.render('t2', {infectionData: infectionData});
     }
 }
