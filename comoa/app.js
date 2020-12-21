d90b434f88cc569c7d87b7f500ad3329352b24fc
sungkyu <tjdrb1945@gmail.com>
2020-12-22 03:25:07 +0900

    123

--- comoa/app.js
+++ comoa/app.js
@@ -26,8 +26,55 @@ var replyRouter = require('./routes/replys');
 var covidRouter = require('./routes/covid');
 var fileRouter = require('./routes/file');
 
+//
+var chatSo = require('./routes/testChat');
+//
+
 var app = express();
 app.use(layouts);
+
+//socket
+const chatDb = require('./mongoDB/chatMongo');
+const testMO = require('./mongoDB/infectionStatusMongo');
+
+let a = 0;
+let tempname = '';
+app.io = require('socket.io')();
+
+var socketids=[];
+app.io.on('connection', function(socket){
+    
+     socket.on('login', async (tempdata)=>{
+         console.log(tempdata);
+        chatDb.chatList().then(function(result){
+            result.forEach(item =>{
+                app.io.to(socket.id).emit('Omessage', item.name, item.msg);
+              })
+        });
+        tempname = "익명" + a++;  
+        socket.name = tempname;
+        app.io.to(socket.id).emit('create name', tempname);  
+        console.log(tempname);
+    });    
+
+    // var chatlog = await chatDb.chatList();
+    // console.log(chatlog);
+    // for(key in chatlog) {
+    //     app.io.to(socket.id).emit('Omessage', key.name, key.msg);
+    // }
+    console.log("a user connected");
+      
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });     
+    socket.on('message', (name, msg) =>{
+        tempname = name;
+        socket.emit('Mmessage', msg);
+        socket.broadcast.emit('Omessage', name, msg);
+        chatDb.chatInsert([{'name' : name, 'msg' : msg}]);
+    });
+  });
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -81,6 +128,9 @@ app.use('/reply', replyRouter);
 app.use('/covid', covidRouter);
 app.use('/file', fileRouter);
 
+//
+app.use('/chat', chatSo);
+//
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     next(createError(404));
@@ -96,5 +146,9 @@ app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error');
 });
+
+
+
+
 
 module.exports = app;
