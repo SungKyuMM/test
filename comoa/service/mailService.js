03f09c3c25cec5217599999b03a0696aacc879a7
joonhyeok <dkttkemf@gmail.com>
2020-12-22 13:13:04 +0900

    main

--- comoa/service/mailService.js
+++ comoa/service/mailService.js
@@ -5,13 +5,13 @@ module.exports = async (email, res, dice) => {
     let transport = mailer.createTransport({
         service: 'gmail',
         auth: {
-            user: '이메일',
-            pass: '패스워드'
+            user: 'dkttkemf@gmail.com',
+            pass: 'Alclstorl1!'
         }
     });
 
     let mailOptions = {
-        from: '이메일',
+        from: 'dkttkemf@gmail.com',
         to: email,
         subject: 'CoNav 회원 인증 이메일 입니다.',
         text: `귀하의 인증번호는 ${dice} 입니다. \n받으신 인증번호를 입력하시면 다음으로 넘어갑니다.`
