74eb24a216f0bdf7ae8408e35b8cabbb5712ca5b
leejongsoo1 <63297609+leejongsoo1@users.noreply.github.com>
2021-04-10 17:13:18 +0900

    Update mailService.js

--- comoa/service/mailService.js
+++ comoa/service/mailService.js
@@ -6,16 +6,13 @@ module.exports = async (email, res, dice) => {
     let transport = mailer.createTransport({    // 메일 발신인 정보
         service: 'gmail',                       // 이용 메일
         auth: {
-            // user: 'dkttkemf@gmail.com',         // 발신인 이메일
-            // pass: 'Alclstorl1!'                 // 발신인 패스워드
-            user: 'jongsoo996@gmail.com',         // 발신인 이메일
-            pass: 'bfmvhjriemgjhczv' 
+            user: '이메일',         // 발신인 이메일
+            pass: '패스워드'              // 발신인 패스워드
         }
     });
 
     let mailOptions = {                         // 메일 내용 정보
-        // from: 'dkttkemf@gmail.com',             // 발신인 이메일
-        from: 'jongsoo996@gmail.com', 
+        from: 'jongsoo996@gmail.com',           // 발신인 이메일
         to: email,                              // 수신인 이메일
         subject: 'Comoa 회원 인증 이메일 입니다.',// 메일 제목
         text: `귀하의 인증번호는 ${dice} 입니다. \n받으신 인증번호를 입력하시면 다음으로 넘어갑니다.` // 메일 내용
